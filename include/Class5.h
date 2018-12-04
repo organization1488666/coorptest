66a08205ef28360f28711de2502aa608881c63c9
arkhamlord666 <arkhamlord666@gmail.com>
2018-12-04 19:14:27 +0000

    class 5 added by arkham666

---
+++ include/Class5.h
@@ -0,0 +1,16 @@
+#ifndef CLASS5_H
+#define CLASS5_H
+
+
+class Class5
+{
+    public:
+        Class5();
+        virtual ~Class5();
+
+    protected:
+
+    private:
+};
+
+#endif // CLASS5_H
---
+++ src/Class5.cpp
@@ -0,0 +1,11 @@
+#include "Class5.h"
+
+Class5::Class5()
+{
+    //ctor
+}
+
+Class5::~Class5()
+{
+    //dtor
+}
