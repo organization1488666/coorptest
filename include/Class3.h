15ac30dc52b17b178fb8b31351a0edde3825d58d
user1488666 <tiago.c.s.1859@gmail.com>
2018-12-04 15:03:49 +0000

    class 3 added by master

---
+++ include/Class3.h
@@ -0,0 +1,16 @@
+#ifndef CLASS3_H
+#define CLASS3_H
+
+
+class Class3
+{
+    public:
+        Class3();
+        virtual ~Class3();
+
+    protected:
+
+    private:
+};
+
+#endif // CLASS3_H
---
+++ src/Class3.cpp
@@ -0,0 +1,11 @@
+#include "Class3.h"
+
+Class3::Class3()
+{
+    //ctor
+}
+
+Class3::~Class3()
+{
+    //dtor
+}
