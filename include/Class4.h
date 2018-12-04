24f95e33a06d4bd4054a7828ba8359518169a913
user1488666 <tiago.c.s.1859@gmail.com>
2018-12-04 15:04:57 +0000

    class 4 added by master

---
+++ include/Class4.h
@@ -0,0 +1,16 @@
+#ifndef CLASS4_H
+#define CLASS4_H
+
+
+class Class4
+{
+    public:
+        Class4();
+        virtual ~Class4();
+
+    protected:
+
+    private:
+};
+
+#endif // CLASS4_H
---
+++ src/Class4.cpp
@@ -0,0 +1,11 @@
+#include "Class4.h"
+
+Class4::Class4()
+{
+    //ctor
+}
+
+Class4::~Class4()
+{
+    //dtor
+}
