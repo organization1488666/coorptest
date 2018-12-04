8c7730873c60ec428e63c496a051853d85318d06
john <arkhamlord666@gmail.com>
2018-12-04 19:20:59 +0000

    module 2 started

---
+++ include/M2_Class1.h
@@ -0,0 +1,16 @@
+#ifndef M2_CLASS1_H
+#define M2_CLASS1_H
+
+
+class M2_Class1
+{
+    public:
+        M2_Class1();
+        virtual ~M2_Class1();
+
+    protected:
+
+    private:
+};
+
+#endif // M2_CLASS1_H
